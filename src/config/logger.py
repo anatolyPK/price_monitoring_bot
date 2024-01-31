67d580db62099b4081d77cc31ef3ed5dc935cc7c
Anatoly <89687109+anatolyPK@users.noreply.github.com>
2024-01-31 20:22:35 +1000

    Создал основную структуру приложения

---
+++ src/config/logger.py
@@ -0,0 +1,19 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(name, level=logging.DEBUG, log_file_path="logs/information.log", max_bytes=10485760, backup_count=5):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(lineno)s %(message)s')
+
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=max_bytes, backupCount=backup_count)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.monitoring.product_parser import start_parse
+
+
+def main():
+    start_parse()
+
+
+if __name__ == '__main__':
+    main()
