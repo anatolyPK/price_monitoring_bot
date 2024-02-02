0e92077a5e122b3ae301f25de68fddb2c5605320
Anatoly <89687109+anatolyPK@users.noreply.github.com>
2024-02-03 09:26:40 +1000

    Добавил ветку свою

--- src/monitoring/product_parser.py
+++ src/monitoring/product_parser.py
@@ -1,27 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 from ..config.logger import setup_logger
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
+import time
+import random
+
 
 logger = setup_logger(__name__)
 
 
 def start_parse():
-    url = 'https://www.ozon.ru/product/nastolnaya-igra-zelenyy-mir-1362989901/'
-    response = requests.get(url)
+    user_agent = UserAgent()
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'user-agent={user_agent.random}')
+    # options.add_argument('--proxy-server=138.128.91.65:8000')
 
-    logger.debug(response.text)
+    driver = webdriver.Chrome(options=options)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+    url = 'https://megamarket.ru/catalog/?q=iphone%2015%20128'
 
-        price_elements = soup.select('.18m')  # Замените это на актуальный CSS-селектор вашего сайта
-        logger.debug(price_elements)
+    try:
+        driver.get(url=url)
 
-        if price_elements:
-            price = price_elements[0].text.strip()
-            logger.debug(price)
-            return price
-        else:
-            return "Цена не найдена"
+        time.sleep(3)
+        elements = driver.find_elements(By.CLASS_NAME, 'item-price')
+
+        prices = []
+        for element in elements:
+            elemement_string_price = element.get_attribute("innerText")
+            element_int_price = parse_price_string(elemement_string_price)
+            prices.append(element_int_price)
+        logger.debug(prices)
+    except Exception as ex:
+        print(ex)
+    else:
+        driver.close()
+        driver.quit()
 
 
+def parse_price_string(price_string):
+    price_string = price_string.replace('\xa0', ' ')
+    price_string = ''.join(c for c in price_string if c.isdigit() or c.isspace())
+    price = int(price_string.replace(' ', ''))
+    return price
