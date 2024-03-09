f1e6452ed4995b4637c61eaf8178ad3a52c190d0
Anatoly <89687109+anatolyPK@users.noreply.github.com>
2024-03-09 16:28:38 +1000

    Раз в час

--- config/database_config.py
+++ config/database_config.py
@@ -9,10 +9,14 @@ import os
 load_dotenv()
 
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.environ['DB_USER']
+# DB_USER = os.getenv('DB_USER')
+DB_PASS = os.environ['DB_PASS']
+# DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.environ['DB_HOST']
+# DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.environ['DB_NAME']
+# DB_NAME = os.getenv('DB_NAME')
 
 if None in [DB_USER, DB_PASS, DB_HOST, DB_NAME]:
     raise ValueError("One or more required environment variables are missing.")
