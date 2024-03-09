5e9bf166ae14e044da91bb7c0844638e535fe75b
Anatoly <89687109+anatolyPK@users.noreply.github.com>
2024-03-09 19:35:23 +1000

    Раз в час

--- config/database_config.py
+++ config/database_config.py
@@ -12,7 +12,8 @@ logger = setup_logger(__name__)
 
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
-DB_HOST = os.getenv('DB_HOST')
+# DB_HOST = os.getenv('DB_HOST')
+DB_HOST = os.getenv('HOSTNAME')
 DB_NAME = os.getenv('DB_NAME')
 
 logger.debug(os.environ.keys())
